dd8ee1c399d69057964fe4698f6bac79793a8543
Bakar Qureshi <qureshimuhammadabubakar@gmail.com>
2026-01-25 19:43:15 +0500

    Create 02-Data-Type-in-Constant.py

---
+++ TensorFlow/Tensors/Using Constants/02-Data-Type-in-Constant.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+vector = tf.constant([[3., 2.],
+                     [4., 5.],
+                     [3, 4]], dtype= tf.float16) 
+'''                             Specify the data type with dtype parameter                     
+'''
+
+print(vector)
