bee80ee538f09486b54d6ef985aa9a45451ad156
Bakar Qureshi <qureshimuhammadabubakar@gmail.com>
2026-01-25 19:43:22 +0500

    Create 00-TensorFlow-Fundamentals.py

---
+++ TensorFlow/Tensors/Using Constants/00-TensorFlow-Fundamentals.py
@@ -0,0 +1,15 @@
+'''     import tensorflow 
+'''
+
+import tensorflow as tf
+
+'''     Checking version 
+'''
+print(tf.__version__)
+
+
+'''     create tensors with tf.constant() 
+'''
+scalar = tf.constant(1)
+
+print(scalar)
