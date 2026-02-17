50952055c3ee507d27d7ce0daacddcb61b7f03ba
Bakar Qureshi <qureshimuhammadabubakar@gmail.com>
2026-02-17 15:55:11 +0500

    Create data_setup.py

---
+++ PyTorch/going_modular/going_modular/data_setup.py
@@ -0,0 +1,35 @@
+
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader
+
+def create_dataloaders(
+        train_dir: str,
+        test_dir: str,
+        transform: transforms.Compose,
+        batch_size: int,
+        num_workers: int
+):
+
+    train_data = datasets.ImageFolder(root=train_dir,
+                                      transform=transform)
+
+    test_data = datasets.ImageFolder(root=test_dir,
+                                     transform=transform)
+
+    classes =  train_data.classes
+
+    train_dataloader = DataLoader(
+        dataset=train_data,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        shuffle=True
+    )
+
+    test_dataloader = DataLoader(
+        dataset=test_data,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        shuffle=False
+    )
+
+    return train_dataloader, test_dataloader, classes
